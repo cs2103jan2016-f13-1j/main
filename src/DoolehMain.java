651d36d0e48c270eae4897438b62a143efca14e0
Tyson <quektyson@gmail.com>
2016-02-19 15:49:26 +0800

    Add Main class

---
+++ src/DoolehMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author pixelducky
+ *
+ */
+public class DoolehMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
