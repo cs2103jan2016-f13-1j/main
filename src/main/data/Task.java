899605c8ce2480eb05f58a3d377bf65c6a953b01
Jxxxxx <x3muogu@hotmail.com>
2016-02-23 02:13:59 +0800

    update task, toString method incomplete

--- src/main/data/Task.java
+++ src/main/data/Task.java
@@ -1,72 +1,129 @@
-/**
- * 
- */
 package main.data;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
- * @author Joleen
+ * @author Joleeen
  *
  */
+
 public class Task {
-
-	private String title;
-	private String startDate;
-	private String startTime;
-	private String endDate;
-	private String endTime;
-	private String label;
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(String startDate) {
-		this.startDate = startDate;
-	}
-
-	public String getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(String startTime) {
-		this.startTime = startTime;
-	}
-
-	public String getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(String endDate) {
-		this.endDate = endDate;
-	}
-
-	public String getEndTime() {
-		return endTime;
-	}
-
-	public void setEndTime(String endTime) {
-		this.endTime = endTime;
-	}
-
-	public String getLabel() {
-		return label;
-	}
-
-	public void setLabel(String label) {
-		this.label = label;
-	}
-	
-	//returns different strings based on fields that tasks has
-	public String toString() {
-		return title + " from " + startDate + " " + startTime + " to "
-				+ endDate + " " + endTime + " label: " + label;
-	}
+    //required
+    private String title;
+    
+    //optional
+    private int status; //01 undone done
+    private int priority; //0123 no LMH
+    private boolean isPostpone;
+    private boolean isRecurring;
+    private ArrayList<String> labels;
+    private Date startDate;
+    private Date endDate;
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public int getStatus() {
+        return status;
+    }
+    
+    public int getPriority() {
+        return priority;
+    }
+    
+    public boolean getIsPostpone(){
+        return isPostpone;
+    }
+    
+    public boolean getIsRecurring() {
+        return isRecurring;
+    }
+    
+    public ArrayList<String> getLabels() {
+        return labels;
+    }
+    
+    public Date getStartDate() {
+        return startDate;
+    }
+    
+    public Date getEndDate() {
+        return endDate;
+    }
+    
+    @Override
+    public String toString() {
+        int startDateOnly = startDate.getDate();
+        int startTime = startDate.getHours();
+        int endDateOnly = endDate.getDate();
+        int endTime = endDate.getHours();
+        return title + " from " + startDateOnly + " " + startTime + " to " + endDateOnly + " " + endTime + " label: " + labels;
+    }
+    
+    private Task(TaskBuilder builder) {
+        this.title = builder.title;
+        this.status = builder.status;
+        this.priority = builder.priority;
+        this.isPostpone = builder.isPostpone;
+        this.isRecurring = builder.isRecurring;
+        this.labels = builder.labels;
+        this.startDate = builder.startDate;
+        this.endDate = builder.endDate;
+    }
+    
+    public static class TaskBuilder {
+        private String title;
+        private int status;
+        private int priority;
+        private boolean isPostpone;
+        private boolean isRecurring;
+        private ArrayList<String> labels;
+        private Date startDate;
+        private Date endDate;
+        
+        public TaskBuilder (String title) {
+            this.title = title;
+        }
+        
+        public TaskBuilder setStatus(int status) {
+            this.status = status;
+            return this;
+        }
+        
+        public TaskBuilder setPriority(int priority) {
+            this.priority = priority;
+            return this;
+        }
+        
+        public TaskBuilder setPostpone(boolean isPostpone) {
+            this.isPostpone = isPostpone;
+            return this;
+        }
+        
+        public TaskBuilder setRecurring(boolean isRecurring) {
+            this.isRecurring = isRecurring;
+            return this;
+        }
+        
+        public TaskBuilder setLabels(ArrayList<String> labels) {
+            this.labels = labels;
+            return this;
+        }
+        
+        public TaskBuilder setStartDate(Date startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+        
+        public TaskBuilder setEndDate(Date endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+        
+        public Task build() {
+            return new Task(this);
+        }
+    }
 }
