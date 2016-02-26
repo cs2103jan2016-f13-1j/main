2ba123b6ed41468983d359a4ad140851b8b58d29
Tyson Quek Yang Sheng <quektyson@gmail.com>
2016-02-26 17:06:13 +0800

    Add ability to capture user input instantly

--- src/main/ui/RootLayoutController.java
+++ src/main/ui/RootLayoutController.java
@@ -6,8 +6,12 @@ import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import main.data.TaskBean;
 import main.logic.ControllerStub;
 
@@ -16,28 +20,44 @@ public class RootLayoutController {
 
 	private ObservableList<String> taskList = FXCollections.observableArrayList();
 
-	
-	@FXML // fx:id="allView"
-    private ListView<String> allView; // Value injected by FXMLLoader
-	
+	@FXML
+	private Label labelFeedback;
 
+	@FXML
+	private TextField commandBar;
+
+	@FXML // fx:id="allView"
+	private ListView<String> allView; // Value injected by FXMLLoader
+	
+	ControllerStub controller;
 
 	public RootLayoutController() {
 	}
 
 	@FXML
 	private void initialize() {
-	
+
 		ControllerStub controller = new ControllerStub();
-		ListProperty<String> lp =  new SimpleListProperty<String>();
+		ListProperty<String> lp = new SimpleListProperty<String>();
 		allView.itemsProperty().bind(lp);
 		ArrayList<TaskBean> tasks = controller.getTasks();
-		for(TaskBean tb : tasks){
+		for (TaskBean tb : tasks) {
 			taskList.add(tb.getTitle());
 		}
 		lp.set(taskList);
 		
+		commandBar.setOnKeyPressed(new EventHandler<Event>() {
+
+			@Override
+			public void handle(Event event) {
+				// TODO Auto-generated method stub
+				labelFeedback.setText(commandBar.getText());
+				
+			}
+		});
+
 	}
+
 
 	/**
 	 * Is called by the main application to give a reference back to itself.
