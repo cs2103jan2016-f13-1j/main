8fb4350387414475562276e5c366081ee4a9a126
Tyson <quektyson@gmail.com>
2016-03-04 00:39:50 +0800

    Add ability for user to select task using up/down arrow keys

--- src/main/ui/DoolehMainApp.java
+++ src/main/ui/DoolehMainApp.java
@@ -1,7 +1,6 @@
 package main.ui;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,49 +8,52 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class DoolehMainApp extends Application {
-	private Stage primaryStage;
-	private AnchorPane rootLayout;
-	
-	
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
 
-	@Override
-	public void start(Stage primaryStage) {
-		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Dooleh");
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Dooleh");
 
-		initRootLayout();
-	}
+        initRootLayout();
+    }
 
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(DoolehMainApp.class.getResource("/main/resources/layouts/RootLayout.fxml"));
 
-	/**
-	 * Initializes the root layout.
-	 */
-	public void initRootLayout() {
-		try {
-			// Load root layout from fxml file.
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(DoolehMainApp.class.getResource("/main/resources/layouts/RootLayout.fxml"));
-			rootLayout = (AnchorPane) loader.load();
+            rootLayout = (AnchorPane) loader.load();
 
-			// Show the scene containing the root layout.
-			Scene scene = new Scene(rootLayout);
-			primaryStage.setScene(scene);
-			primaryStage.show();				
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+            //get a handle on the UI controller and set focus to the text field
+            RootLayoutController rootLayoutController = (RootLayoutController) loader.getController();
+            rootLayoutController.requestFocusForCommandBar();
 
-	/**
-	 * Returns the main stage.
-	 * 
-	 * @return
-	 */
-	public Stage getPrimaryStage() {
-		return primaryStage;
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    /**
+     * Returns the main stage.
+     * 
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/ui/RootLayoutController.java
+++ src/main/ui/RootLayoutController.java
@@ -2,6 +2,8 @@ package main.ui;
 
 import java.util.ArrayList;
 
+import javax.xml.stream.EventFilter;
+
 import com.jfoenix.controls.JFXListView;
 
 import javafx.beans.property.ListProperty;
@@ -13,138 +15,174 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import main.data.Task;
 import main.logic.Controller;
-import main.logic.Controller.Tab;
 
 public class RootLayoutController {
-	private DoolehMainApp main;
+    private DoolehMainApp main;
 
-	private ObservableList<String> taskList = FXCollections.observableArrayList();
+    private ObservableList<String> taskList = FXCollections.observableArrayList();
 
-	@FXML
-    private Label labelUserAction;
+    @FXML // fx:id="rootLayout"
+    private AnchorPane rootLayout; // Value injected by FXMLLoader
+
+    @FXML // fx:id="listView"
+    private JFXListView<String> listView; // Value injected by FXMLLoader
+
+    @FXML // fx:id="commandBar"
+    private TextField commandBar; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelCurrentMode"
+    private Label labelCurrentMode; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelUserAction"
+    private Label labelUserAction; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelUserFeedback"
+    private Label labelUserFeedback; // Value injected by FXMLLoader
+
+    private ArrayList<Task> allTasks;
+    private Controller controller;
+    private String inputFeedback;
+    private String userInput;
+    private String userCommand;
+
+    public RootLayoutController() {
+
+    }
 
     @FXML
-    private Label labelUserFeedback;
+    private void initialize() {
 
+        Controller controller = new Controller();
 
-	@FXML
-	private TextField commandBar;
+        // ListView seems to only allow binding of a OberservableList of String
+        // type
+        ListProperty<String> lp = new SimpleListProperty<String>();
+        listView.itemsProperty().bind(lp);
 
-	@FXML // fx:id="allView"
-	private ListView<String> allView; // Value injected by FXMLLoader
-	
-	private Controller controller;
-	private String inputFeedback;
-	private String userInput;
-	private String userCommand;
-	
-	public RootLayoutController() {
-	}
+        // retrieve all task and add into an ObservableList
+        allTasks = controller.getAllTasks();
+        for (Task task : allTasks) {
+            taskList.add(task.toString());
+        }
+        lp.set(taskList);
 
-	@FXML
-	private void initialize() {
+        rootLayout.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
 
-		Controller controller = new Controller();
-		
-		//ListView seems to only allow binding of a OberservableList of String type
-		ListProperty<String> lp = new SimpleListProperty<String>();
-		allView.itemsProperty().bind(lp);
-		
-		//retrieve all task and add into an ObservableList
-		ArrayList<Task> tasks = controller.getAllTasks();
-		for (Task task : tasks) {
-			taskList.add(task.toString());
-		}
-		lp.set(taskList);
-		
-		commandBar.setOnKeyReleased(new EventHandler<Event>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                // TODO Auto-generated method stub
 
-			@Override
-			public void handle(Event event) {
-				// TODO Auto-generated method stub
-				userInput = commandBar.getCharacters().toString();
-				
-				if(userInput.length() == 0){
-					labelUserFeedback.setVisible(false);
-					labelUserAction.setVisible(false);
-					userInput = "";
-					labelUserAction.setText("");
-					labelUserFeedback.setText("");
-					return;
-				}
-				
-				//stub method to grab command type from user input
-				userCommand = userInput.split(" ")[0];
-				switch(userCommand){
-				case "search":
-				case "find":
-					labelUserFeedback.setVisible(true);
-					labelUserAction.setText("Searching:");
-					labelUserAction.setVisible(true);
-					break;
-					
-				case "delete":
-				case "del":
-					labelUserFeedback.setVisible(true);
-					labelUserAction.setText("Deleting:");
-					labelUserAction.setVisible(true);
-					break;
-					
-				default:
-					labelUserFeedback.setVisible(true);
-					labelUserAction.setText("Adding:");
-					labelUserAction.setVisible(true);
-					
-				}
-				
-				inputFeedback = controller.parseCommand(userInput, Tab.NO_TAB);
-				
-				labelUserAction.setVisible(true);
-				labelUserFeedback.setText(inputFeedback);
-				labelUserFeedback.setVisible(true);
-				System.out.println(inputFeedback);
-				
-			}
-		});
-		
-		commandBar.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent event) {
-				// TODO Auto-generated method stub
-				controller.executeCommand();
-				
-				//clear and retrieve all task and add into an ObservableList
-				taskList.clear();
-				ArrayList<Task> tasks = controller.getAllTasks();
-				for (Task task : tasks) {
-					taskList.add(task.toString());
-				}
-				
-				//if we dont set the list again, the listview item may show a buggy arrangement
-				//due to how the listview recycles it's cell items for effiency
-				lp.set(taskList);
-				commandBar.clear();
-				
-				labelUserFeedback.setVisible(false);
-				labelUserAction.setVisible(false);
-			}
-		});
+                // pass focus over to listview instead of tabs
+                if (keyEvent.getCode() == KeyCode.UP) {
+                    listView.getSelectionModel().selectPrevious();
+                    keyEvent.consume();
 
-	}
+                } else if (keyEvent.getCode() == KeyCode.DOWN) {
+                    listView.getSelectionModel().selectNext();
+                    keyEvent.consume();
+                } else if (keyEvent.getCode() == KeyCode.F1) {
+                    if (labelCurrentMode.getText().equals("Edit mode")) {
+                        labelCurrentMode.setText("Today");
 
+                    } else {
+                        labelCurrentMode.setText("Edit mode");
+                    }
+                    keyEvent.consume();
+                }
 
-	/**
-	 * Is called by the main application to give a reference back to itself.
-	 * 
-	 * @param mainApp
-	 */
-	public void setMainApp(DoolehMainApp mainApp) {
-		this.main = mainApp;
+                System.out.println(keyEvent.getTarget());
 
-	}
+            }
+        });
+
+        commandBar.setOnKeyReleased(new EventHandler<Event>() {
+
+            @Override
+            public void handle(Event event) {
+                // TODO Auto-generated method stub
+                userInput = commandBar.getCharacters().toString();
+
+                if (userInput.length() == 0) {
+                    labelUserFeedback.setVisible(false);
+                    labelUserAction.setVisible(false);
+                    userInput = "";
+                    labelUserAction.setText("");
+                    labelUserFeedback.setText("");
+                    return;
+                }
+
+                // stub method to grab command type from user input
+                userCommand = userInput.split(" ")[0];
+                switch (userCommand) {
+                    case "search" :
+                    case "find" :
+                        labelUserFeedback.setVisible(true);
+                        labelUserAction.setText("Searching:");
+                        labelUserAction.setVisible(true);
+                        break;
+
+                    case "delete" :
+                    case "del" :
+                        labelUserFeedback.setVisible(true);
+                        labelUserAction.setText("Deleting:");
+                        labelUserAction.setVisible(true);
+                        break;
+
+                    default :
+                        labelUserFeedback.setVisible(true);
+                        labelUserAction.setText("Adding:");
+                        labelUserAction.setVisible(true);
+
+                }
+
+                inputFeedback = controller.parseCommand(userInput, Controller.Tab.NO_TAB);
+
+                labelUserAction.setVisible(true);
+                labelUserFeedback.setText(inputFeedback);
+                labelUserFeedback.setVisible(true);
+                System.out.println(inputFeedback);
+
+            }
+        });
+
+        commandBar.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                // TODO Auto-generated method stub
+                controller.executeCommand();
+
+                // clear and retrieve all task and add into an ObservableList
+                taskList.clear();
+                ArrayList<Task> tasks = controller.getAllTasks();
+                for (Task task : tasks) {
+                    taskList.add(task.toString());
+                }
+
+                // if we dont set the list again, the listview item may show a
+                // buggy arrangement
+                // due to how the listview recycles it's cell items for effiency
+                lp.set(taskList);
+                commandBar.clear();
+
+                labelUserFeedback.setVisible(false);
+                labelUserAction.setVisible(false);
+            }
+        });
+
+        //requestFocus will not work here since this class is run before
+        //before the construction of the scene in DoolehMainApp class
+//        commandBar.requestFocus();
+    }
+    
+    public void requestFocusForCommandBar(){
+        commandBar.requestFocus();
+    }
 }
