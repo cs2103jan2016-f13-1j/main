ef9e056b5637d788626f7efd1c6c0703cf286b46
Quek Yang Sheng Tyson <quektyson@gmail.com>
2016-03-13 19:44:52 +0800

    Update layout design for ListView cells

---
+++ src/main/ui/CustomListCellController.java
@@ -0,0 +1,94 @@
+package main.ui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+import main.data.Task;
+
+public class CustomListCellController extends ListCell<Task> {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="horizontalBox"
+    private HBox horizontalBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelTaskIndex"
+    private Label labelTaskIndex; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelTaskTime"
+    private Label labelTaskTime; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelTaskTitle"
+    private Label labelTaskTitle; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelTaskTag"
+    private Label labelTaskTag; // Value injected by FXMLLoader
+
+    public CustomListCellController() {
+        // TODO Auto-generated constructor stub
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/resources/layouts/CustomListCell.fxml"));
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void updateItem(Task item, boolean empty) {
+        // TODO Auto-generated method stub
+        super.updateItem(item, empty);
+
+        if (empty) {
+            setGraphic(null);
+        } else {
+            setLabelTaskIndex(item);
+            setLabelTaskTitle(item);
+            setLabelTaskTime(item);
+            setLabelTaskTag(item);
+            setGraphic(getHorizontalBox());
+        }
+    }
+
+    public HBox getHorizontalBox() {
+        return horizontalBox;
+    }
+
+    public void setLabelTaskIndex(Task task) {
+//        this.labelTaskIndex.setText(task.getId());
+    }
+
+    public void setLabelTaskTime(Task task) {
+        this.labelTaskTime.setText(task.getStartDate() + " - " + task.getEndDate());
+    }
+
+    public void setLabelTaskTitle(Task task) {
+        this.labelTaskTitle.setText(task.getTitle());
+    }
+
+    public void setLabelTaskTag(Task task) {
+        if(task.hasLabel()){
+            this.labelTaskTag.setText(task.getLabel());   
+            this.labelTaskTag.setVisible(true);
+        }
+        else{
+            this.labelTaskTag.setVisible(false);
+        }
+
+        
+
+    }
+
+}
