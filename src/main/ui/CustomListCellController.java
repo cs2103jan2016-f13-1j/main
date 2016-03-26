1a36777a3f56935939d88aa0b2855f255f8ce993
Quek Yang Sheng Tyson <quektyson@gmail.com>
2016-03-26 19:30:42 +0800

    Update GUI with new label and priority colors (high, med, low)

--- src/main/ui/CustomListCellController.java
+++ src/main/ui/CustomListCellController.java
@@ -8,19 +8,22 @@ import com.jfoenix.controls.JFXListCell;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import main.data.Task;
 
 public class CustomListCellController extends JFXListCell<Task> {
 
+    private static final String PRIORITY_LOW = "#FFC107";
+    private static final String PRIORITY_MED = "#FF9800";
+    private static final String PRIORITY_HIGH = "#FF5252";
+    private static final String PRIORITY_NONE = "#FF5252";
+
     @FXML // fx:id="horizontalBox"
     private HBox horizontalBox; // Value injected by FXMLLoader
-    
-    @FXML // fx:id="verticalBox"
-    private VBox verticalBox; // Value injected by FXMLLoader
 
     @FXML // fx:id="labelTaskTime"
     private JFXButton labelTaskTime; // Value injected by FXMLLoader
@@ -31,8 +34,8 @@ public class CustomListCellController extends JFXListCell<Task> {
     @FXML // fx:id="labelTaskIndex"
     private Label labelTaskIndex; // Value injected by FXMLLoader
 
-    @FXML // fx:id="flowPaneTitleAndDate"
-    private FlowPane flowPaneTitleAndDate; // Value injected by FXMLLoader
+    @FXML // fx:id="verticalBox"
+    private VBox verticalBox; // Value injected by FXMLLoader
 
     @FXML // fx:id="labelTaskTitle"
     private Label labelTaskTitle; // Value injected by FXMLLoader
@@ -40,12 +43,29 @@ public class CustomListCellController extends JFXListCell<Task> {
     @FXML // fx:id="labelTaskDate"
     private Label labelTaskDate; // Value injected by FXMLLoader
 
-    @FXML // fx:id="labelTaskTag"
-    private Label labelTaskTag; // Value injected by FXMLLoader
+    @FXML // fx:id="labelTaskLabel"
+    private Label labelTaskLabel; // Value injected by FXMLLoader
+
+    @FXML // fx:id="rectangleTaskPriority"
+    private Rectangle rectangleTaskPriority; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is
+          // complete
+    void initialize() {
+        assert horizontalBox != null : "fx:id=\"horizontalBox\" was not injected: check your FXML file 'CustomListCellLayout.fxml'.";
+        assert labelTaskTime != null : "fx:id=\"labelTaskTime\" was not injected: check your FXML file 'CustomListCellLayout.fxml'.";
+        assert circleIndex != null : "fx:id=\"circleIndex\" was not injected: check your FXML file 'CustomListCellLayout.fxml'.";
+        assert labelTaskIndex != null : "fx:id=\"labelTaskIndex\" was not injected: check your FXML file 'CustomListCellLayout.fxml'.";
+        assert verticalBox != null : "fx:id=\"verticalBox\" was not injected: check your FXML file 'CustomListCellLayout.fxml'.";
+        assert labelTaskTitle != null : "fx:id=\"labelTaskTitle\" was not injected: check your FXML file 'CustomListCellLayout.fxml'.";
+        assert labelTaskDate != null : "fx:id=\"labelTaskDate\" was not injected: check your FXML file 'CustomListCellLayout.fxml'.";
+        assert labelTaskLabel != null : "fx:id=\"labelTaskLabel\" was not injected: check your FXML file 'CustomListCellLayout.fxml'.";
+        assert rectangleTaskPriority != null : "fx:id=\"rectangleTaskPriority\" was not injected: check your FXML file 'CustomListCellLayout.fxml'.";
+
+    }
 
     public CustomListCellController() {
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/main/resources/layouts/CustomListCellLayout.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main/resources/layouts/CustomListCellLayout.fxml"));
         loader.setController(this);
         try {
             loader.load();
@@ -61,11 +81,12 @@ public class CustomListCellController extends JFXListCell<Task> {
         if (empty) {
             setGraphic(null);
         } else {
-            setLabelTaskIndex(getIndex() + 1);
+            showTaskIndex(getIndex() + 1);
             setLabelTaskTitle(task);
-            setLabelTaskTime(task);
-            setLabelTaskDate(task);
-            setLabelTaskTag(task);
+            showTaskTime(task);
+            showTaskDate(task);
+            showTaskLabel(task);
+            showTaskPriority(task);
             setGraphic(getHorizontalBox()); // HBox is the parent layout of all
                                             // the other UI components here.
                                             // Returning an instance of this
@@ -78,11 +99,11 @@ public class CustomListCellController extends JFXListCell<Task> {
         return horizontalBox;
     }
 
-    public void setLabelTaskIndex(int index) {
+    public void showTaskIndex(int index) {
         this.labelTaskIndex.setText(index + "");
     }
 
-    public void setLabelTaskTime(Task task) {
+    public void showTaskTime(Task task) {
         if (task.getSimpleTime().isEmpty()) {
             System.out.println(task.getSimpleTime());
             this.labelTaskTime.setText("-");
@@ -95,26 +116,47 @@ public class CustomListCellController extends JFXListCell<Task> {
         this.labelTaskTitle.setText(task.getTitle());
     }
 
-    public void setLabelTaskDate(Task task) {
-        System.out.println(task.getSimpleDate());
-        if (task.getSimpleDate().isEmpty()) {
+    public void showTaskDate(Task task) {
+        if (!task.hasDate()) {
+            System.out.println("Task has no date");
             verticalBox.getChildren().remove(this.labelTaskDate);
             return;
         }
-        if(!verticalBox.getChildren().contains(this.labelTaskDate)){
+
+        this.labelTaskDate.setText(task.getSimpleDate());
+
+        if (!verticalBox.getChildren().contains(this.labelTaskDate)) {
             verticalBox.getChildren().add(this.labelTaskDate);
         }
-        this.labelTaskDate.setText(task.getSimpleDate());
+
     }
 
-    public void setLabelTaskTag(Task task) {
+    public void showTaskLabel(Task task) {
         if (task.hasLabel()) {
-            this.labelTaskTag.setText(task.getLabel());
-            this.labelTaskTag.setVisible(true);
+            this.labelTaskLabel.setText(task.getLabel());
+            this.labelTaskLabel.setVisible(true);
         } else {
-            this.labelTaskTag.setVisible(false);
+            this.labelTaskLabel.setVisible(false);
+        }
+    }
+
+    public void showTaskPriority(Task task) {
+        if (task.getPriority() == 1) {
+            rectangleTaskPriority.setFill(Color.web(PRIORITY_LOW));
+            return;
         }
 
+        if (task.getPriority() == 2) {
+            rectangleTaskPriority.setFill(Color.web(PRIORITY_MED));
+            return;
+        }
+
+        if (task.getPriority() == 3) {
+            rectangleTaskPriority.setFill(Color.web(PRIORITY_HIGH));
+            return;
+        }
+
+        rectangleTaskPriority.setFill(Color.TRANSPARENT);
     }
 
 }
