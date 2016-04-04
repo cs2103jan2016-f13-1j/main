257e0a8009f125722463a81472ad73bd5f16cea1
Bevin Seetoh Jia Jin <a0134234@u.nus.edu>
2016-04-04 18:49:27 +0800

    Implement priority sort for floating task, and if equal time

--- src/main/logic/CustomComparator.java
+++ src/main/logic/CustomComparator.java
@@ -30,14 +30,20 @@ import main.data.Task;
 class TodoTaskComparator implements Comparator<Task> {
     public int compare(Task t1, Task t2) {
         if (!t1.hasDate() && !t2.hasDate()) {
-            //If both are floating tasks, compare the created date
-            return t1.getCreatedDate().compareTo(t2.getCreatedDate());
+            //If both are floating tasks
+            if (t1.getPriority() == t2.getPriority()) {
+                //If equal priority, compare the created date
+                return t1.getCreatedDate().compareTo(t2.getCreatedDate());
+            } else {
+                //Return higher priority first
+                return t2.getPriority() - t1.getPriority();
+            }
         } else if (!t1.hasDate() && t2.hasDate()) {
             //If one is floating and the other is dated, return the floating task
-            return -1;
+            return 1;
         } else if (t1.hasDate() && !t2.hasDate()) {
             //If one is dated and the other is floating, return the floating task
-            return 1;
+            return -1;
         } else {
             //Both tasks has date
             if (t1.hasSingleDate() && t2.hasSingleDate()) {
@@ -57,7 +63,7 @@ class TodoTaskComparator implements Comparator<Task> {
                     }
                 }
             } else if (t1.hasDateRange() && t2.hasSingleDate()) {
-              //If one is ranged and the other has single date
+                //If one is ranged and the other has single date
                 if (t1.hasStarted()) {
                     //t1 have stared
                     return t1.getEndDate().compareTo(t2.getSingleDate());
@@ -74,8 +80,14 @@ class TodoTaskComparator implements Comparator<Task> {
                 if (t1.hasStarted() && t2.hasStarted()) {
                     //If both tasks already started
                     if (t1.getEndDate().compareTo(t2.getEndDate()) == 0) {
-                        //If both tasks has same end date, compare the created date
-                        return t1.getCreatedDate().compareTo(t2.getCreatedDate());
+                        //If both tasks has same end date
+                        if (t1.getPriority() == t2.getPriority()) {
+                            //If equal priority, compare the created date
+                            return t1.getCreatedDate().compareTo(t2.getCreatedDate());
+                        } else {
+                            //Return higher priority first
+                            return t2.getPriority() - t1.getPriority();
+                        }
                     } else {
                         //If both tasks has different end date, return earlier deadline first
                         return t1.getEndDate().compareTo(t2.getEndDate());
@@ -84,10 +96,23 @@ class TodoTaskComparator implements Comparator<Task> {
                     //t1 has started but not t2
                     return -1;
                 } else if (!t1.hasStarted() && t2.hasStarted()) {
+                    //t2 has started but not t1
                     return 1;
                 } else {
                     //If both have not started yet
-                    return t1.getStartDate().compareTo(t2.getStartDate());
+                    if (t1.getStartDate().compareTo(t2.getStartDate()) == 0) {
+                        //If both have the same start date
+                        if (t1.getPriority() == t2.getPriority()) {
+                            //If equal priority, compare the created date
+                            return t1.getCreatedDate().compareTo(t2.getCreatedDate());
+                        } else {
+                            //Return higher priority first
+                            return t2.getPriority() - t1.getPriority();
+                        }
+                    } else {
+                        //Return earlier start date first
+                        return t1.getStartDate().compareTo(t2.getStartDate());
+                    }
                 }
             }
         }
